da28f6d6633781ab6d6591b0b51502d3c91c3785
erzya <erzyasd@gmail.com>
2016-02-11 23:02:56 +0200

    New archObjType page

--- resources/static/components/cms/archObjectType/archObjectTypeController.js
+++ resources/static/components/cms/archObjectType/archObjectTypeController.js
@@ -1,15 +1,23 @@
 'use strict';
 
 var cms_ArchObjectTypeController = angular.module('cms_ArchObjectTypeController',[]);
+var archObjectListMock =[{"id_arch":1,"name":"колонна","description":"высокая","constructingDate":"10/10/2010"}
+                    , {"id_arch":2,"name":"урна","description":"широкая","constructingDate":"01/11/2005"}
+                    , {"id_arch":3,"name":"балясина","description":"толстая","constructingDate":"10/10/1950"}
+                    ,{"id_arch":4,"name":"Крест","description":"православный","constructingDate":"10/10/2010"}
+                    , {"id_arch":5,"name":"орган","description":"не православный","constructingDate":"01/11/2005"}
+                    , {"id_arch":6,"name":"столб","description":"стоит","constructingDate":"10/10/1950"}];
 
-var arrTypeMock = [{"name":"Доходный дом", "parent":"квартал","archObjects":
-                                                                            [{"name":"колонна","description":"высокая","constructingDate":"10/10/2010"}
-                                                                                , {"name":"урна","description":"широкая","constructingDate":"01/11/2005"}
-                                                                                , {"name":"балясина","description":"толстая","constructingDate":"10/10/1950"}]}
-                                                                            ,{"name":"Сабор", "parent":"квартал","archObjects":
-                                                                                [{"name":"Крест","description":"православный","constructingDate":"10/10/2010"}
-                                                                                    , {"name":"орган","description":"не православный","constructingDate":"01/11/2005"}
-                                                                                    , {"name":"столб","description":"стоит","constructingDate":"10/10/1950"}]}];
+var archObjectList1 = [[{"id_arch":1,"name":"колонна","description":"высокая","constructingDate":"10/10/2010"}
+                        , {"id_arch":2,"name":"урна","description":"широкая","constructingDate":"01/11/2005"}
+                        , {"id_arch":3,"name":"балясина","description":"толстая","constructingDate":"10/10/1950"}]
+                       ,[{"id_arch":4,"name":"Крест","description":"православный","constructingDate":"10/10/2010"}
+                        , {"id_arch":5,"name":"орган","description":"не православный","constructingDate":"01/11/2005"}
+                        , {"id_arch":6,"name":"столб","description":"стоит","constructingDate":"10/10/1950"}]];
+
+
+var arrTypeMock = [{"id_arch_type":1,"name":"Доходный дом", "parent":"квартал","archObjects":archObjectList1[0]}
+                    ,{"id_arch_type":1,"name":"Сабор", "parent":"квартал","archObjects":archObjectList1[1]}];
 
 cms_ArchObjectTypeController.controller('CMS_ArchObjTypeControllerList',['$scope', function($scope) {
     $scope.tableName="Тип архитектурного объекта";
@@ -18,8 +26,12 @@ cms_ArchObjectTypeController.controller('CMS_ArchObjTypeControllerList',['$scope
 
 cms_ArchObjectTypeController.controller('CMS_ArchObjTypeControllerNew',['$scope', function($scope) {
     $scope.tableName="Создание нового типа архитектурного объекта";
+    $scope.archObjectList = archObjectListMock;
     $scope.listArchObjTypes = arrTypeMock;
+    $scope.listType = [{"id":1,"name":"квартал"},{"id":2,"name":"район"},{"id":3,"name":"дом"},{"id":4,"name":"помещение"}];
     $scope.saveArchObjType = function(){
-        console.log("listSize    ", $scope.listArchObjTypes.length);
+        console.log("name    ", $scope.archTypeObj.name);
+        console.log("parent    ", $scope.archTypeObj.parent);
+        console.log("archObjs    ", $scope.archTypeObj.archObjects);
     }
 }]);
--- resources/static/components/cms/archObjectType/archObjectTypeNew.jsp
+++ resources/static/components/cms/archObjectType/archObjectTypeNew.jsp
@@ -7,28 +7,31 @@
         <div ng-class="{ 'has-error' : archTypeObjForm.name.$invalid && !archTypeObjForm.name.$pristine }">
             <label>Наименование</label>
             <input type="text" name="name" class="form-control" ng-model="archTypeObj.name" required ng-minlength="3" ng-maxlength="80">
-            <p ng-show="archTypeObjForm.name.$error.minlength" class="help-block">Client name is too short.</p>
-            <p ng-show="archTypeObjForm.name.$error.maxlength" class="help-block">Client name is too long.</p>
+            <p ng-show="archTypeObjForm.name.$error.minlength" class="help-block">Имя слишком короткое</p>
+            <p ng-show="archTypeObjForm.name.$error.maxlength" class="help-block">Имя слишком длинное</p>
+            <br/>
         </div>
 
         <!-- Parent Type-->
-        <div ng-class="{ 'has-error' : archTypeObjForm.phone.$invalid && !archTypeObjForm.phone.$pristine }">
+        <div ng-class="{ 'has-error' : archTypeObjForm.parent.$invalid && archTypeObjForm.parent.$touched }">
             <label>Тип объекта</label>
-            <input type="text" name="phone" class="form-control" ng-model="archTypeObj.parent" required />
-            <p ng-show="archTypeObjForm.phone.$error.pattern" class="help-block">Enter valid number!</p>
+            <select class="form-control" name="parent" ng-model="archTypeObj.parent" ng-options="parent.name for parent in listArchObjTypes" required>
+                <option value="">Выбрать тип</option>
+            </select>
+            <br/>
         </div>
 
         <!-- archObject -->
-        <div ng-class="{'has-error' : archTypeObjForm.client.$invalid && !archTypeObjForm.name.$pristine}">
+        <div ng-class="{'has-error' : archTypeObjForm.archObj.$invalid && archTypeObjForm.archObj.$touched }">
             <label>Список объектов</label>
-            <select class="form-control" name="archObj" ng-model="archTypeObj.archObjects" ng-options="archObjects.name for client in listClients" required>
-                <option value="">Выбрать объектt</option>
+            <select class="form-control" name="archObj" ng-model="archTypeObj.archObjects" ng-options="archObj.name for archObj in archObjectList" required>
+                <option value="">Выбрать объект</option>
             </select>
             <br/>
         </div>
 
         <!-- SUBMIT BUTTON -->
-        <button type="submit" class="btn btn-primary">Submit</button>
+        <button type="submit" class="btn btn-primary" ng-disabled="archTypeObjForm.$invalid">Сохранить</button>
 
     </form>
 </div>
