349d5cd9df993be2c1d5e11f4cced2d7d7f08d1a
Sergey Morenets <morenets@mail.ru>
2016-02-24 19:32:03 +0200

    Adding modal dialogs

--- resources/static/components/cms/archObjectType/archObjectTypeApp.js
+++ resources/static/components/cms/archObjectType/archObjectTypeApp.js
@@ -1,7 +1,36 @@
 'use strict';
 
-angular.module('cms_ArchObjectTypeApp',['ui.router','cms_ArchObjectTypeController'])
-    .config(['$stateProvider',function($stateProvider){
+var app = angular.module('cms_ArchObjectTypeApp',['ui.router', 'ui.bootstrap','cms_ArchObjectTypeController']);
+
+app.provider('modalState', ['$stateProvider', function($stateProvider) {
+    var provider = this;
+    this.$get = function() {
+        return provider;
+    }
+    this.state = function(stateName, options) {
+        var modalInstance;
+        $stateProvider.state(stateName, {
+            url: options.url,
+            onEnter: ['$uibModal', '$state', function($modal, $state) {
+                modalInstance = $modal.open(options);
+                modalInstance.result['finally'](function() {
+                    modalInstance = null;
+                    if ($state.$current.name === stateName) {
+                        $state.go('^');
+                    }
+                });
+            }],
+            onExit: function() {
+                if (modalInstance) {
+                    modalInstance.close();
+                }
+            }
+        });
+     };
+  }
+]);
+
+    app.config(['$stateProvider', 'modalStateProvider',  function($stateProvider, modalStateProvider){
         $stateProvider
             .state('cms.archObjectType', {
                 url: '/archObjectType',
@@ -12,16 +41,34 @@ angular.module('cms_ArchObjectTypeApp',['ui.router','cms_ArchObjectTypeControlle
                 templateUrl: '/resources/static/components/cms/archObjectType/archObjectTypeList.html',
                 controller : 'CMS_ArchObjTypeControllerList'
             })
-            .state('cms.archObjectType.new', {
+            /*.state('cms.archObjectType.new', {
                 url: '/new',
                 templateUrl: '/resources/static/components/cms/archObjectType/archObjectTypeNew.html',
                 controller : 'CMS_ArchObjTypeControllerNew'
-            })
+            })*/
             .state('cms.archObjectType.del', {
                 url: '/del/:objType_id',
                 templateUrl: '/resources/static/components/cms/archObjectType/archObjectTypeList.html',
                 controller : 'CMS_ArchObjTypeControllerDel'
-            })
+            });
+
+         modalStateProvider.state('cms.archObjectType.list.new', {
+    	    url: '/new',
+            templateUrl: '/resources/static/components/cms/archObjectType/archObjectTypeNew.html',
+            controller : 'CMS_ArchObjTypeControllerNew',
+    	    data: {
+    	      roles: []
+    	    }             
+	 });
+         modalStateProvider.state('cms.archObjectType.list.edit', {
+    	    url: '/new',
+            templateUrl: '/resources/static/components/cms/archObjectType/archObjectTypeNew.html',
+            controller : 'CMS_ArchObjTypeControllerNew',
+    	    data: {
+    	      roles: []
+    	    }             
+	 });
+
 
         /*$routeProvider
             .when('/cms/archObjectTypeList',
@@ -41,3 +88,4 @@ angular.module('cms_ArchObjectTypeApp',['ui.router','cms_ArchObjectTypeControlle
             })*/
         }
     ]);
+
